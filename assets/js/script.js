9bb6b7dab1bafb6b41724df2b8fe306b5584ad41
mmelendez3 <mele0019@gmail.com>
2021-09-05 09:23:49 -0500

    four random generator data types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,51 @@
 // Assignment code here
 
+  //input variables
+  var length;
+  var inputType = {
+  lower: generateLowerCase(),
+  upper: generateUpperCase(),
+  number: generateNumber(),
+  symbol: generateSymbol()
+}
+
 // Four random generator data types
     
     //lowercase
     function generateLowerCase() {
-      const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+      var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
     
       return lowerAlphabet[Math.floor(Math.random() * lowerAlphabet.length)];
-      console.log(generateLowerCase())
+      
     }
+    console.log(generateLowerCase())
     
-        //uppercase
+    //uppercase
     function generateUpperCase() {
-      const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
       return upperAlphabet[Math.floor(Math.random() * upperAlphabet.length)];
-      console.log(generateUpperCase())
+      
     }
+    console.log(generateUpperCase())
     
-        //numbers
+    //numbers
     function generateNumber() {
-      const number = "0123456789"
+      var number = "0123456789"
     
       return number[Math.floor(Math.random() * number.length)];
-      console.log(generateNumber())
+      
     }
+    console.log(generateNumber())
     
-        //special character
-    function generateSpecialCharacter() {
-      const character = "!@#$%^&*()"
+    //symbols
+    function generateSymbol() {
+      var symbol = "!@#$%^&*()"
     
-      return character[Math.floor(Math.random() * character.length)];
-      console.log(generateSpecialCharacter())
+      return symbol[Math.floor(Math.random() * symbol.length)];
+      
     }
+    console.log(generateSymbol())
 
 
 
@@ -47,52 +60,43 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
+
+  //ask user how long they want password be and use while loop to ensure they input valid option
+  var length = Number(prompt("Password must be between 8 and 128 characters. How long would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Please enter valid number between 8 - 128!"));
+
+    
+  //ask user which input types they want to include in their password and use while loop to ensure at least one option is chosen
+  var upperCase = confirm("Would you like to use uppercase letters?");
+  var lowerCase = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+
+  while (!upperCase && !lowerCase && !numbers && !symbols) {
+    alert("You must select at least one password variable!");
+    upperCase = confirm("Would you like to use uppercase letters?");
+    lowerCase = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+  }
+        
+        
    
-  //ask user how long they want password
-var promptLength = (prompt("How long do you want your password to be? Please choose between 8 - 128 characters!"));{
+
   
-
-//validate prompt answer
-
-//if nothing entered then return message asking for valid response
-if (promptLength === "" || promptLength === null) {
-  window.alert("You need to provide a valid answer! Please Try Again.");
-
-  return writePassword();
-}
-//if answer is less than 8 or more than 128 alert them to length conditions
-else if (promptLength < 8) {
-  window.alert("Password must be at least 8 characters long!  Please Try Again.")
-
-  return writePassword();
-}
-else if (promptLength > 128) {
-  window.alert("Password must be less than 128 characters long! Please Try Again.")
-
-  return writePassword();
-}
-//if they dont input a number then return alert informing user that only numeric values are accepted
-while (promptLength !== parseInt(promptLength).toString()) {
-  alert("Please enter only numbers between 8 - 128!");
-
-  return writePassword();
-}
-
-};
-
-var inputType = (alert("Please select the character types to include in password!"));
+   
+    }
   
-  window.confirm("Do you want lowercase characters included in your password?");
   
-  window.confirm("Do you want uppercase characters included in your password?")
-  window.confirm("Do you want numbers included in your password?")
-  window.confirm("Do you want special characters included in your password?")
+  
+    
 
+      
+      
+      
+      
+     
 
-if (inputType == false) {
-  window.alert("You must select at least one variable for your password!")
-  return writePassword();
-}
 
 
 
@@ -115,7 +119,7 @@ if (inputType == false) {
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
